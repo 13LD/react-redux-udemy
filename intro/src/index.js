ac8ac7ad91cce2c1f9c56622f9ad86724cb1d678
Dmytro Lysohor <tompla96@ukr.net>
2018-03-31 01:07:10 +0300

    multiple components on 1 render

--- intro/src/index.js
+++ intro/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+var app = (
+  <div>
+    <App name="Max" year="28" />
+    <App name="Manu" year="29" />
+  </div>
+);
 
-ReactDOM.render(<App name="Dimas" year="21" />, document.getElementById('p1'));
+ReactDOM.render(app, document.getElementById('p1'));
 registerServiceWorker();
