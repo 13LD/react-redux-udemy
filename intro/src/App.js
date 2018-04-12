f663c172b13ab764fbc1e011729a9ddc7d0bbcc7
Dmytro Lysohor <tompla96@ukr.net>
2018-04-12 17:23:04 +0300

    event handling

--- intro/src/App.js
+++ intro/src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 import Person from './Person/Person'
 class App extends Component {
 
+  state = {
+    persons: [
+      {name: "Dima", age: 21},
+      {name: "Sasha", age: 21},
+      {name: "Max", age: 20}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log("Was clicked!")
+    // DON'T DO THIS:  this.state.persons[0].name = 'Dmytro'
+    this.setState({
+      persons: [
+        {name: "Dmytro", age: 21},
+        {name: "Sasha", age: 21},
+        {name: "Max", age: 20}
+      ]
+    })
+  }
   render() {
 
     return (
       <div className="App">
         <h1>Hi, i'm react</h1>
         <p>This is working</p>
-        <Person name="Dmytro" age="21"> My Hobbies: reading, travelling</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> My Hobbies: reading, travelling</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
