12a93ab3d2e55ad5a17248eda6b7ef6915fe7c6b
Dmytro Lysohor <tompla96@ukr.net>
2018-04-12 15:00:52 +0300

    functional component

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person'
 class App extends Component {
 
   render() {
 
     return (
-      <div className="person">
-        <h1>{this.props.name}</h1>
-        <p>Your years: {this.props.year}</p>
+      <div className="App">
+        <h1>Hi, i'm react</h1>
+        <p>This is working</p>
+        <Person/>
       </div>
     );
   }
--- intro/src/index.js
+++ intro/src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var app = (
-  <div>
-    <App name="Max" year="28" />
-    <App name="Manu" year="29" />
-  </div>
-);
 
-ReactDOM.render(app, document.getElementById('p1'));
+ReactDOM.render(<App />, document.getElementById('p1'));
 registerServiceWorker();
